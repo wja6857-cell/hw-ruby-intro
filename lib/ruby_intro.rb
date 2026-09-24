97c245dd3fb717b95312e8d77fe9c1bb02de4297
William Abraham <williamjohnabraham1@gmail.com>
2026-09-24 11:35:23 -0400

    homework submission

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,128 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  ans = 0
+  arr.each do |num|
+    ans += num
+  end
+  ans
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+
+  num1 = -Float::INFINITY
+  num2 = -Float::INFINITY
+
+  arr.each do |num|
+    if num > num1
+      num2 = num1
+      num1 = num
+    elsif num > num2
+      num2 = num
+    end
+  end
+
+  num1 + num2
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  h = {}
+
+  arr.each_with_index do |num, index|
+    h[num] = index
+  end
+
+  arr.each_with_index do |num, index|
+    cand = n - num
+    if h[cand] and h[cand] != index
+      return true
+    end
+  end
+
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  constants = {
+    "A" => true, "E" => true, "I" => true, "O" => true, "U" => true,
+    "a" => true, "e" => true, "i" => true, "o" => true, "u" => true
+  }
+
+  if s.length == 0
+    return false
+  end
+
+  if s.match?(/^[a-zA-Z]/)
+    if constants[s[0]]
+      return false
+    else
+      return true
+    end
+  end
+
+  false
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  # not valid binary
+  if s.match?(/[^01]/) or s.length == 0
+    return false
+  end
+
+  decimal = 0
+  val = 1
+  s.reverse.each_char do |char|
+    decimal += char.to_i * val
+    val *= 2
+  end
+
+  decimal % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.length == 0
+      raise ArgumentError, "ISBN is an empty string"
+    end
+
+    if price <= 0
+      raise ArgumentError, "Price is less than or equal to zero"
+    end
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
+  def isbn=(arg)
+    @isbn = arg
+  end
+
+  def price=(arg)
+    @price = arg
+  end
+
+  def price_as_string
+    "$" + format("%.2f", @price)
+  end
 end
